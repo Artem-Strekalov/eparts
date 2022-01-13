0b0f29b7a88d0b776b3672823cbe5a6e3c61b3cd
Artem-Strekalov <strekalovjob@gmail.com>
2022-01-13 18:04:45 +0300

    work

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Auth.scss";
 import TextField from "@mui/material/TextField";
-import { alpha, styled } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 
 const Auth = () => {
   const MyInput = styled(TextField)({
-    width: "300px",
+    width: "333px",
     background: "white",
     borderRadius: "10px",
     "& .MuiOutlinedInput-input": {
@@ -18,6 +18,7 @@ const Auth = () => {
       },
     },
   });
+
   return (
     <div className="content">
       <div className="content__header">
@@ -25,7 +26,7 @@ const Auth = () => {
       </div>
       <div className="content__main">
         <form>
-          <h2>Авторизация</h2>
+          <h2>Авторизация на Eparts</h2>
           <div className="content__input">
             <p>Введите e-mail</p>
             <MyInput
@@ -44,8 +45,12 @@ const Auth = () => {
               id="custom-css-outlined-input"
             />
           </div>
+          <button className="content__form-btn">Войти</button>
         </form>
-        <div className="content__main-img"></div>
+        <img
+          className="content__main-img"
+          src={process.env.PUBLIC_URL + "/image/png/tractor.png"}
+        />
       </div>
     </div>
   );
