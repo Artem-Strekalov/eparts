6a1d17e0ced04843385565fe507a1d7e079942f4
Artem-Strekalov <strekalovjob@gmail.com>
2022-01-22 22:52:37 +0300

    work

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,17 @@
 import {initializeApp} from 'firebase/app'
+import {
+  getDatabase,
+  ref,
+  onValue,
+  orderByChild,
+  get,
+  equalTo,
+  query,
+  startAt,
+  startAfter,
+  endAt,
+} from 'firebase/database'
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBlPdlZI4QBis52d6l0QJGa9DhtKCzZfwo',
   authDomain: 'eparts-4de00.firebaseapp.com',
@@ -8,4 +21,13 @@ const firebaseConfig = {
   appId: '1:504331053119:web:c93cbac045ec18264fb08a',
 }
 const appFirebase = initializeApp(firebaseConfig)
+
+const db = getDatabase(appFirebase)
+const dbRef = ref(db, 'products')
+const getData = async () => {
+  const data = await get(query(dbRef, orderByChild('name'), equalTo('118370')))
+  console.log(data.val())
+  return data.val()
+}
+getData()
 export default appFirebase
