56410482f24292f6a4bb167b741b5c20562b78f6
Artem-Strekalov <strekalovjob@gmail.com>
2022-01-24 21:42:57 +0300

    search done

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import {initializeApp} from 'firebase/app'
-import {
-  getDatabase,
-  ref,
-  onValue,
-  orderByChild,
-  get,
-  equalTo,
-  query,
-  startAt,
-  startAfter,
-  endAt,
-} from 'firebase/database'
+import {getFirestore} from 'firebase/firestore'
+import {collection, getDocs, where, query} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBlPdlZI4QBis52d6l0QJGa9DhtKCzZfwo',
@@ -21,13 +11,21 @@ const firebaseConfig = {
   appId: '1:504331053119:web:c93cbac045ec18264fb08a',
 }
 const appFirebase = initializeApp(firebaseConfig)
+const db = getFirestore()
 
-const db = getDatabase(appFirebase)
-const dbRef = ref(db, 'products')
-const getData = async () => {
-  const data = await get(query(dbRef, orderByChild('name'), equalTo('118370')))
-  console.log(data.val())
-  return data.val()
+//поиск работает, разберись с поиском по нескольким словам
+/* const ref = doc(db, 'eparts') */
+
+async function getData() {
+  const querySnapshot = await query(
+    collection(db, 'eparts'),
+    where('keys.45180', '==', true),
+  )
+  const test = await getDocs(querySnapshot)
+  test.forEach((doc) => {
+    console.log(doc.data())
+  })
 }
+
 getData()
 export default appFirebase
